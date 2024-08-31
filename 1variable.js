09a6df87ab54861561a394f5fed730975b7c5a37
ALIRAZAASIM <casim9530@gmail.com>
2024-08-31 13:20:50 +0000

    container config done

---
+++ 1variable.js
@@ -0,0 +1,9 @@
+const name = "ali raza"
+let email = "casim@gmail.com";
+var password = 1234;
+
+/* we dont prefer var bcz block scope 
+and function problem in var keyword
+*/
+
+console.table([name,email,password]);
