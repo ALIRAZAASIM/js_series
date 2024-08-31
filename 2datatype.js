9d4254bce8fdb8c953e0043726fe346e91102b1a
ALIRAZAASIM <casim9530@gmail.com>
2024-08-31 17:07:40 +0000

    container config done

---
+++ 2datatype.js
@@ -0,0 +1,30 @@
+// //data type in js 
+
+// let data ={
+//     name :"ali",
+//     rollno : 123,
+//     isregulara : true,
+// };
+// console.log(data);
+
+// // string=>"ali "   //name 
+// // number=> 1234   // digits
+// // bolean =>true or false // true false
+// // null=>stand alone ,empty value
+// // undefine => value available but we dont know the value
+// //symbol=> uniqueness 
+
+//  objects
+
+// console.log(typeof "name");//string 
+
+
+// console.log(null>0);//false
+// console.log(null>=0);//true 
+
+
+console.log("2"==2);//true string convert to number
+console.log("2"===2);//false bcz equalateral === string not convert
+
+
+
