b35d96002c1c97636d3a3897fbeb2673d04ba455
ALIRAZAASIM <casim9530@gmail.com>
2024-09-02 22:28:36 +0000

    container config done

---
+++ 4string.js
@@ -0,0 +1,45 @@
+// var name = "ali";
+// let regId = 123 ;
+
+// console.log(`my name is  ${name} and my id is ${regId}`);
+
+// console.log(name[0]);//a bcz string no start from 0 .
+
+// console.log(name.length);//3
+
+// console.log(name.toUpperCase()); //ALI
+
+
+// console.log(name.toLowerCase());//ali
+
+// console.log(name.charAt(2));//i
+
+// console.log(name.indexOf("a"));//0
+
+// var name = "aliraza";
+// let regId = 123456 ;
+
+// let newName = name.slice(0,4)//alir
+// console.log(newName);
+
+// let newName =          "ali"
+// console.log(newName.trim()); //trim used to remove  space from start and last.
+
+// console.log(name.replace("ali","raza"));//razaraza replce the value
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
