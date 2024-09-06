d2f40ed2c47c279d76a5a33444d9fedcc1a6c6dd
ALIRAZAASIM <casim9530@gmail.com>
2024-09-06 16:15:57 +0000

    container config done

---
+++ 6dates.js
@@ -0,0 +1,25 @@
+let myDate = new Date();
+
+// console.log(myDate.toDateString()); // friday sep 06 2024
+
+// console.log(myDate.toLocaleString()); //  9/6/2024 ,3:22:50pm
+
+// console.log(myDate.getMonth());
+
+// console.log(myDate.getDay());
+
+// myDate.toLocaleString('default',{
+//     weekday:"long"
+// })
+
+// let mystampvalue = Date.now()
+// console.log(mystampvalue);
+
+
+// console.log(Date.now()/1000); //in mili second 1727387.013
+
+// console.log(Math.floor(Date.now()/1000)) /// 17273387
+
+
+ 
+
