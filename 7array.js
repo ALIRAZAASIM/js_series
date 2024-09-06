94b124bb5b998494fb3326ee3339a6a5c88b5ee1
ALIRAZAASIM <casim9530@gmail.com>
2024-09-06 21:53:22 +0000

    container config done

---
+++ 7array.js
@@ -0,0 +1,54 @@
+let array = [1,2,3,4,5]
+
+// console.log(array);
+
+// console.log(array.length);//5 length of its
+
+// console.log(array[1]);//2 
+
+// array.push(6);
+// console.log(array);//add 6 in arrray
+
+// array.pop()
+// console.log(array);//  pop delete last value 
+
+let num = [1,2,3,4,5];
+
+// num.unshift(3)
+// console.log(num);//unshift add new value at the start of array
+
+// num.shift()
+// console.log(num);// shift delete the first value of array 
+
+
+// push and pop add and delete last from array    and  unshift and shift start from array
+
+
+// let newnum = num.join();
+// console.log(newnum);// result come without [] brackets and in string 
+
+// console.log(typeof newnum);//string 
+
+
+//splice and slice in js 
+
+let arr1 = [1,2,3,4,5]
+// console.log("A",arr1);
+
+// arr2 = arr1.slice(2,4)
+// console.log(arr2);// 3,4 result slice means a piece 
+
+
+// console.log("B",arr1);
+// arr3 = arr1.splice(2,4)
+// console.log(arr3); // add and remove ele from array [3,4,5] result 
+
+
+
+
+
+
+
+
+
+
