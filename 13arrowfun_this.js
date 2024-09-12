58c6bc06e4a49ed6b6ef835124b8afed3113970c
ALIRAZAASIM <casim9530@gmail.com>
2024-09-12 20:03:11 +0000

    container config done

---
+++ 13arrowfun_this.js
@@ -0,0 +1,36 @@
+// const sum = function num(num1,num2){
+//     return num1 + num2
+// }
+//console.log(sum(3,4));return 7 
+
+//++++++++++++++++Arrow function ++++++++++
+
+// const sum = (num1,num2)=>{
+//     return num1 +num2
+// }
+
+// console.log(sum(3,4));//return 7        return keyword used means expleset
+
+
+// const sum = (num1,num2)=> num1+num2
+
+// console.log(sum(3,4));//7 this called impleset bcz return keyword dont used
+
+// const sum = (num1,num2)=> (num1+num2) //with paranthesis
+
+// console.log(sum(3,4));//7 this called impleset bcz return keyword dont used
+
+
+//++++++++++++ arrow function with objects++++++++++++++++++++++//
+
+const sum = ()=>({name :"aliraza"})// add paranthesis used object
+
+console.log(sum());
+
+
+
+//+++++++++++++++++++++this. ++++++++++++++++//
+
+//search on chatgpt about it 
+//in short this refers to the context in which a function is executed. Its value depends on how and where the function is called:
+
