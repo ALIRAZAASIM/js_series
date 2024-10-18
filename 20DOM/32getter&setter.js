12810a96183dff21fb35bde0a7d1efe3d6d7dc69
ALIRAZAASIM <casim9530@gmail.com>
2024-10-18 10:09:49 +0000

    main container config

---
+++ 20DOM/32getter&setter.js
@@ -0,0 +1,32 @@
+// getter and setter using class 
+
+//getter and setter always using both 
+
+// name  of those are same in it 
+
+// _ or # used after this. 
+
+class User {
+    constructor(email, password){
+        this.email = email;
+        this.password = password
+    }
+
+    get email(){
+        return this._email.toUpperCase()
+    }
+    set email(value){
+        this._email = value
+    }
+
+    get password(){
+        return `${this._password}hitesh`
+    }
+
+    set password(value){
+        this._password = value
+    }
+}
+
+const hitesh = new User("h@hitesh.ai", "abc")
+console.log(hitesh.email);
